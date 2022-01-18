51f09a3169786563e8c6b998d749d79c83424816
Parkune <88872275+Parkune@users.noreply.github.com>
2022-01-18 20:10:02 +0900

    test

---
+++ Assets/02.Script/ClearManager.cs
@@ -0,0 +1,33 @@
+using System.Security.Cryptography.X509Certificates;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClearManager : MonoBehaviour
+{
+
+    public List<GameObject> enemyList = new List<GameObject>();
+    [SerializeField]
+    public GameObject[] enemys;
+    // Start is called before the first frame update
+    void Start()
+    {
+       enemys = GameObject.FindGameObjectsWithTag("ENEMY");
+    }
+    void enemyDie(string enemyName)
+    {
+        //에너미가 죽으면 리스트의 오브젝트들을 비교한다.
+        for()
+        {
+
+        }
+        //내 이름과 리스트에 담긴 이름이 같다면 리스트에서 제거한다.
+
+        //if 리스트의 카운트가 0이 되면 클리어를 프린트한다.
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
