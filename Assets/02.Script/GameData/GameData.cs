6ea87ea566bbe2e403aea7d89677dca8efda4e94
Park Kun <88872275+Parkune@users.noreply.github.com>
2022-01-20 00:31:55 +0900

    LocalDataSave

---
+++ Assets/02.Script/GameData/GameData.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[SerializeField]
+public class GameData
+{
+    //0 켜짐 상태,1 음소거 상태
+    public int BGMSound = 0;
+    public int EffectSound = 0;
+
+    // 각챕터의 잠금여부
+    public bool isClear1_1;
+    public bool isClear1_2;
+    public bool isClear1_3;
+    public bool isClear1_4;
+    public bool isClear1_5;
+    public bool isClear2_1;
+    public bool isClear2_2;
+    public bool isClear2_3;
+    public bool isClear2_4;
+    public bool isClear2_5;
+    public bool isClear3_1;
+    public bool isClear3_2;
+    public bool isClear3_3;
+    public bool isClear3_4;
+    public bool isClear3_5;
+}
