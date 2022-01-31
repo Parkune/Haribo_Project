91c132bd0dbadd9b7edb2453be5e7082fee6d87f
Parkune <88872275+Parkune@users.noreply.github.com>
2022-01-31 22:29:59 +0900

    refair

--- Assets/02.Script/StageManager.cs
+++ Assets/02.Script/StageManager.cs
@@ -54,60 +54,60 @@ public class StageManager : MonoBehaviour
     }
     void FrictionForceData()
     {
-        stageFrictionForce.Add(0, 0.1f);
+       stageFrictionForce.Add(0, 0.2f);
         stageFrictionForce.Add(1, 0.2f);
-        stageFrictionForce.Add(2, 0.3f);
-        stageFrictionForce.Add(3, 0.4f);
-        stageFrictionForce.Add(4, 0.5f);
-        stageFrictionForce.Add(5, 0.6f);
-        stageFrictionForce.Add(6, 0.7f);
-        stageFrictionForce.Add(7, 0.8f);
-        stageFrictionForce.Add(8, 0.8f);
-        stageFrictionForce.Add(9, 0.8f);
-        stageFrictionForce.Add(10, 0.8f);
-        stageFrictionForce.Add(11, 0.8f);
-        stageFrictionForce.Add(12, 0.8f);
-        stageFrictionForce.Add(13, 0.8f);
-        stageFrictionForce.Add(14, 0.8f);
-        stageFrictionForce.Add(15, 0.8f);
-        stageFrictionForce.Add(16, 0.8f);
-        stageFrictionForce.Add(17, 0.8f);
-        stageFrictionForce.Add(18, 0.8f);
-        stageFrictionForce.Add(19, 0.8f);
-        stageFrictionForce.Add(20, 0.8f);
-        stageFrictionForce.Add(21, 0.8f);
-        stageFrictionForce.Add(22, 0.8f);
-        stageFrictionForce.Add(23, 0.8f);
-        stageFrictionForce.Add(24, 0.8f);
+        stageFrictionForce.Add(2, 0.2f);
+        stageFrictionForce.Add(3, 0.2f);
+        stageFrictionForce.Add(4, 0.2f);
+        stageFrictionForce.Add(5, 0.1f);
+        stageFrictionForce.Add(6, 0.1f);
+        stageFrictionForce.Add(7, 0.1f);
+        stageFrictionForce.Add(8, 0.1f);
+        stageFrictionForce.Add(9, 0.1f);
+        stageFrictionForce.Add(10, 0.3f);
+        stageFrictionForce.Add(11, 0.3f);
+        stageFrictionForce.Add(12, 0.3f);
+        stageFrictionForce.Add(13, 0.3f);
+        stageFrictionForce.Add(14, 0.3f);
+        stageFrictionForce.Add(15, 0.4f);
+        stageFrictionForce.Add(16, 0.4f);
+        stageFrictionForce.Add(17, 0.4f);
+        stageFrictionForce.Add(18, 0.4f);
+        stageFrictionForce.Add(19, 0.4f);
+        stageFrictionForce.Add(20, 0.0f);
+        stageFrictionForce.Add(21, 0.0f);
+        stageFrictionForce.Add(22, 0.0f);
+        stageFrictionForce.Add(23, 0.0f);
+        stageFrictionForce.Add(24, 0.0f);
     }
 
     void AddData()
     {
-        Ballct.Add(0, 3);
-        Ballct.Add(1, 3);
-        Ballct.Add(2, 3);
-        Ballct.Add(3, 5);
-        Ballct.Add(4, 5);
-        Ballct.Add(5, 3);
-        Ballct.Add(6, 3);
-        Ballct.Add(7, 3);
-        Ballct.Add(8, 3);
-        Ballct.Add(9, 3);
-        Ballct.Add(10, 3);
-        Ballct.Add(11, 3);
-        Ballct.Add(12, 3);
-        Ballct.Add(13, 3);
-        Ballct.Add(14, 3);
-        Ballct.Add(15, 3);
-        Ballct.Add(16, 3);
-        Ballct.Add(17, 3);
-        Ballct.Add(18, 3);
-        Ballct.Add(19, 3);
-        Ballct.Add(20, 3);
-        Ballct.Add(21, 3);
-        Ballct.Add(22, 3);
-        Ballct.Add(23, 3);
-        Ballct.Add(24, 3);
+        Ballct.Add(0,3);
+        Ballct.Add(1,3);
+        Ballct.Add(2,3);
+        Ballct.Add(3,4);
+        Ballct.Add(4,3);
+        Ballct.Add(5,4);
+        Ballct.Add(6,4);
+        Ballct.Add(7,5);
+        Ballct.Add(8,6);
+        Ballct.Add(9,4);
+        Ballct.Add(10,4);
+        Ballct.Add(11,4);
+        Ballct.Add(12,6);
+        Ballct.Add(13,5);
+        Ballct.Add(14,6);
+        Ballct.Add(15,5);
+        Ballct.Add(16,7);
+        Ballct.Add(17,7);
+        Ballct.Add(18,3);
+        Ballct.Add(19,7);
+        Ballct.Add(20,7);
+        Ballct.Add(21,5);
+        Ballct.Add(22,8);
+        Ballct.Add(23,6);
+        Ballct.Add(24,13);
     }
 
 }
